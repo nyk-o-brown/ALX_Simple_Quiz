f93b8d4be12bf68235c188ba6e981ba965f6cd13
nyk-o-brown <odoyobrown3@gmail.com>
2025-01-19 23:38:05 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 // Attach event listeners for the buttons
-document.getElementById('add').addEventListener('click', function() {
+document.getElementById('+').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
     const result = add(number1, number2);
