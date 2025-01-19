8a788fac3fc34faa8037a6ee580621d7a7882d12
nyk-o-brown <odoyobrown3@gmail.com>
2025-01-19 23:39:23 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -6,21 +6,21 @@ document.getElementById('+').addEventListener('click', function() {
     return document.getElementById('calculation-result').textContent = result;
 });
 
-document.getElementById('subtract').addEventListener('click', function() {
+document.getElementById('-').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
     const result = subtract(number1, number2);
     return document.getElementById('calculation-result').textContent = result;
 });
 
-document.getElementById('multiply').addEventListener('click', function() {
+document.getElementById('*').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
     const result = multiply(number1, number2);
     return document.getElementById('calculation-result').textContent = result;
 });
 
-document.getElementById('divide').addEventListener('click', function() {
+document.getElementById('/').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
     const result = divide(number1, number2);
